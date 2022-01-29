bd734d51be7cb8ef36684cc177714785502244da
lampshed <aperta.no@gmail.com>
2022-01-29 16:59:22 +0000

    initial commit for iss tracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import Map from "./components/Map.js";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [long, setLong] = useState(0.12);
+  const [lati, setLati] = useState(51);
+  const [locked, setLock] = useState(true);
+  const [lockMsg, setTxt] = useState("Unlock from ISS");
+
+  useEffect(() => {
+    getInitLocation();
+  }, []);
+
+  const getInitLocation = async () => {
+    setLoading(true);
+
+    const response = await axios.get("http://api.open-notify.org/iss-now.json");
+    console.log("loading set");
+    const { longitude, latitude } = await response.data.iss_position;
+    setLong(parseFloat(longitude));
+    setLati(parseFloat(latitude));
+    console.log(response);
+    setLoading(false);
+    updateHandler();
+  };
+
+  useEffect(() => {
+    (function selfCall() {
+      updateHandler();
+      setTimeout(selfCall, 2000);
+    })();
+  }, []);
+
+  const updateHandler = async () => {
+    const response = await axios.get("http://api.open-notify.org/iss-now.json");
+    const { longitude, latitude } = await response.data.iss_position;
+    setLong(parseFloat(longitude));
+    setLati(parseFloat(latitude));
+  };
+  const lockOn = () => {
+    if (locked === false) {
+      console.log("locking on");
+      setLock(true);
+      setTxt("Lock to ISS");
+    } else {
+      setLock(false);
+      setTxt("Unlock from ISS");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!loading ? (
+        <>
+          <div className="map-container">
+            <Map
+              issPos={{ lat: lati, lng: long }}
+              center={!locked ? { lat: lati, lng: long } : {}}
+              zoom={4}
+            />
+          </div>
+          <button
+            onClick={() => {
+              lockOn();
+            }}
+            className="lock-on-button"
+          >
+            {lockMsg}
+          </button>
+        </>
+      ) : (
+        <div className="Loading">Loading...</div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Map.js
@@ -0,0 +1,291 @@
+import GoogleMapReact from "google-map-react";
+import ISS from "./iss.png";
+
+function Map({ issPos, center, zoom }) {
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        options={{
+          styles: [
+            {
+              featureType: "all",
+              elementType: "labels",
+              stylers: [
+                {
+                  visibility: "on",
+                },
+              ],
+            },
+            {
+              featureType: "all",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  saturation: 36,
+                },
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 40,
+                },
+              ],
+            },
+            {
+              featureType: "all",
+              elementType: "labels.text.stroke",
+              stylers: [
+                {
+                  visibility: "on",
+                },
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 16,
+                },
+              ],
+            },
+            {
+              featureType: "all",
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "off",
+                },
+              ],
+            },
+            {
+              featureType: "administrative",
+              elementType: "geometry.fill",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 20,
+                },
+              ],
+            },
+            {
+              featureType: "administrative",
+              elementType: "geometry.stroke",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 17,
+                },
+                {
+                  weight: 1.2,
+                },
+              ],
+            },
+            {
+              featureType: "administrative.country",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#e5c163",
+                },
+              ],
+            },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#c4c4c4",
+                },
+              ],
+            },
+            {
+              featureType: "administrative.neighborhood",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#e5c163",
+                },
+              ],
+            },
+            {
+              featureType: "landscape",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 20,
+                },
+              ],
+            },
+            {
+              featureType: "poi",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 21,
+                },
+                {
+                  visibility: "on",
+                },
+              ],
+            },
+            {
+              featureType: "poi.business",
+              elementType: "geometry",
+              stylers: [
+                {
+                  visibility: "on",
+                },
+              ],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.fill",
+              stylers: [
+                {
+                  color: "#e5c163",
+                },
+                {
+                  lightness: "0",
+                },
+              ],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.stroke",
+              stylers: [
+                {
+                  visibility: "off",
+                },
+              ],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#ffffff",
+                },
+              ],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "labels.text.stroke",
+              stylers: [
+                {
+                  color: "#e5c163",
+                },
+              ],
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 18,
+                },
+              ],
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "geometry.fill",
+              stylers: [
+                {
+                  color: "#575757",
+                },
+              ],
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#ffffff",
+                },
+              ],
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "labels.text.stroke",
+              stylers: [
+                {
+                  color: "#2c2c2c",
+                },
+              ],
+            },
+            {
+              featureType: "road.local",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 16,
+                },
+              ],
+            },
+            {
+              featureType: "road.local",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#999999",
+                },
+              ],
+            },
+            {
+              featureType: "transit",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 19,
+                },
+              ],
+            },
+            {
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+                {
+                  lightness: 17,
+                },
+              ],
+            },
+          ],
+        }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+        center={center}
+        zoom={zoom}
+      >
+        <img
+          src={ISS}
+          alt="iss icon"
+          className="iss-icon"
+          lat={issPos.lat}
+          lng={issPos.lng}
+        />
+      </GoogleMapReact>
+    </div>
+  );
+}
+export default Map;
